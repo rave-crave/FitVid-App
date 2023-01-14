34ac1ee769874a9283592d3a24298787a42d8414
monia7dev <monia7dev@gmail.com>
2023-01-14 18:25:01 +0000

    Initial commit

--- GymStreamWorkout.py
+++ GymStreamWorkout.py
@@ -1,1 +1,97 @@
-print("git setup 10 test")
+from datetime import datetime
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7dc999dc13b12c82360a4ca92411f6a9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
+
+class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(20), unique=True, nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        image_file = db.Column(db.String(20), nullable=False, default='jpeg')
+        password = db.Column(db.String(60), nullable=False)
+        new_exercises = db.relationship('NewExercise', backref='instructor', lazy=True)
+
+        def  __repr__(self):
+            return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class NewExercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exercise_name = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def  __repr__(self):
+        return f"Post('{self.exercise_name}', '{self.date_posted}')"
+
+new_exercises = [
+    {   'instructor': 'Matt',
+        'exercise_name': 'Exercise Name 1',
+        'content': 'First new exercise content',
+        'date_posted': 'April 20, 2022'
+    },
+    {   'instructor': 'Emily',
+        'exercise_name': 'Exercise Name 2',
+        'content': 'Second new exercise content',
+        'date_posted': 'May 3, 2022'
+    },
+    {   'instructor': 'Chris',
+        'exercise_name': 'Exercise Name 2',
+        'content': 'Second new exercise content',
+        'date_posted': 'May 21, 2022'
+    },
+    {   'instructor': 'Monika',
+        'exercise_name': 'Exercise Name 2',
+        'content': 'Second new exercise content',
+        'date_posted': 'June 1, 2022'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', new_exercises=new_exercises)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Pelase check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
