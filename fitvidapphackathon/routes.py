8f672d62a64fbce506a0f0f63fbf55e57013d00a
monia7dev <monia7dev@gmail.com>
2023-01-14 21:46:20 +0000

    Change structure

--- fitvidapphackathon/routes.py
+++ fitvidapphackathon/routes.py
@@ -1,40 +1,9 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '7dc999dc13b12c82360a4ca92411f6a9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+from flask import render_template, url_for, flash, redirect
+from fitvidapphackathon import app
+from fitvidapphackathon.forms import RegistrationForm, LoginForm
+from fitvidapphackathon.models import User, NewExercise
 
 
-db = SQLAlchemy(app)
-
-with app.app_context():
-    db.create_all()
-
-class User(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(20), unique=True, nullable=False)
-        email = db.Column(db.String(120), unique=True, nullable=False)
-        image_file = db.Column(db.String(20), nullable=False, default='jpeg')
-        password = db.Column(db.String(60), nullable=False)
-        new_exercises = db.relationship('NewExercise', backref='instructor', lazy=True)
-
-        def  __repr__(self):
-            return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class NewExercise(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    exercise_name = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def  __repr__(self):
-        return f"Post('{self.exercise_name}', '{self.date_posted}')"
 
 new_exercises = [
     {   'instructor': 'Matt',
@@ -92,6 +61,3 @@ def login():
 
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ run.py
@@ -0,0 +1,4 @@
+from fitvidapphackathon import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
