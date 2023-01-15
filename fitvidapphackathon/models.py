1a978477a360278aa227841c8ac73f7744a86177
monia7dev <monia7dev@gmail.com>
2023-01-15 16:48:01 +0000

    Change User model relationship

--- fitvidapphackathon/models.py
+++ fitvidapphackathon/models.py
@@ -7,27 +7,28 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='jpeg')
     password = db.Column(db.String(60), nullable=False)
-    workouts = db.relationship('Workout', backref='instructor', lazy=True)     # research many-to-many relationship
+    workouts = db.relationship('Workout', lazy=True)     # research many-to-many relationship + check whether to add add back_populated or older backref
 
     def  __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Workout(db.Model):
-    id = db.Column(db.Integer, primary_key=True)    #_id   
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)    #_id   
     name = db.Column(db.String(50), nullable=False)
     length = db.Column(db.String(10), nullable=False)
     exercise_type = db.Column(db.String(20), nullable=False)
     intensity = db.Column(db.String(20), nullable=False)
+    instructor = db.Column(db.String(20), nullable=False)
     link = db.Column(db.String(200), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
 
     def  __repr__(self):
-        return f"Exercise('{self.name}', '{self.length}', '{self.exercise_type}', '{self.intensity}','{self.link}')"   
+        return f"Exercise('{self.name}', '{self.length}', '{self.exercise_type}', '{self.intensity}','{self.instructor}''{self.link}')"   
 
 
 
-# class Exercise(db.Model):       for the future if time permits --> individual exercises that might comprise a customised workout plan
+# class Exercise(db.Model):       for the future if time permits --> individual exercises that might comprise a customised workout plan; add instructor
 #     id = db.Column(db.Integer, primary_key=True)    # Matt's ID line:   _id = db.Column("id",db.Integer, primary_key = True);       
 #     name = db.Column(db.String(100), nullable=False)       # Matt's line:    name = db.Column(db.String(50))
 #     # date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
